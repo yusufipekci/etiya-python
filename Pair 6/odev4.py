82ddd790d1dc8e5775a7120c3427e43f4a745ddc
yusufipekci <yusufipekci053@gmail.com>
2022-11-18 15:32:02 +0300

    commit

---
+++ Pair 6/odev4.py
@@ -0,0 +1,24 @@
+employees = open("sample.txt", "a")
+
+try:
+   employeesCount = int(input("çalışan sayısı giriniz: "))
+
+   for i in range(employeesCount):
+        employeesInfo = input("çalışan adı-soyadı giriniz:")
+        employeesSalary = float(input("çalışan maaş bilgisini giriniz:"))
+        employees.write(f"{employeesInfo}, {employeesSalary} \n")
+        total = (f"{employeesInfo} {employeesSalary}")
+        print(total)
+
+except ValueError:
+    print("Ad-Soyad bilgisinde rakam olamaz")
+    print("Maaş bilgisinde string ifade olamaz")
+finally:
+    print("başarılı")
+
+employees.close()
+
+file1 = open("sample.txt","r")
+lines = file1.readlines() 
+for line in lines:
+    print(line)
