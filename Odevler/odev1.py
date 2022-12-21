47de23d67d973f3fcd1abd418593c1670eec0c5c
yusufipekci <yusufipekci053@gmail.com>
2022-12-21 10:19:20 +0300

    update

--- Odevler/odev1.py
+++
@@ -1,9 +0,0 @@
-sayi1 = 5
-sayi2 = 3
-
-if sayi1>sayi2:
-    print(sayi1)
-elif sayi1<sayi2:
-    print(sayi2)
-else:
-    print()
--- Odevler/odev3.py
+++
@@ -1,3 +0,0 @@
-# Altın ve gümüş fiyatlarının yanında yer alan oklar.
-# Canlı Ons, Dolar, Euro ve Gram Altın - Haftalık grafikleri.
-# Al/Sat Simülatörü
